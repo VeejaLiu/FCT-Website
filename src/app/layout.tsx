a6b84d4334a379e497dcec1c0658c300dc0f9e8e
Veeja Liu <veejaliu@gmail.com>
2024-12-20 00:19:17 +0800

    refactor layout component to use Image component and improve structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 // src/app/layout.tsx
-import './globals.css';
-import './fonts.css';
-import NavLink from '@/app/components/NavLink';
-import Link from 'next/link';
-import Footer from './components/Footer';
+import "./globals.css";
+import "./fonts.css";
+import NavLink from "@/app/components/NavLink";
+import Link from "next/link";
+import Footer from "./components/Footer";
+import Image from "next/image";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -15,24 +16,32 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
               <li>
                 <Link href="/">
                   <div className="text-white mr-1 text-xl font-bold cursor-pointer flex items-center">
-                    <img src="/logo.png" alt="Logo" className="h-8 mr-2 bg-white rounded-full" />
-                    <span className='italic'>FC</span>
-                    <span className='italic'>Career</span>
-                    <span className='italic'>.top</span>
+                    <Image
+                      src="/logo.png"
+                      alt="Logo"
+                      width={32}
+                      height={32}
+                      className="h-8 mr-2 bg-white rounded-full"
+                    />
+                    <span className="italic">FC</span>
+                    <span className="italic">Career</span>
+                    <span className="italic">.top</span>
                   </div>
                 </Link>
               </li>
-              <li>
-                <NavLink href="/">Home</NavLink>
-              </li>
-              <li>
-                <NavLink href="/change-logs">Changes</NavLink>
-              </li>
-              <li>
-                <NavLink href="/contact-us">Contact</NavLink>
-              </li>
-              <li className="mr-1">
-                <a href={'https://app.fccareer.top'} target={'_blank'}>
+              <div className="flex items-center space-x-4 flex-grow">
+                <li>
+                  <NavLink href="/">Home</NavLink>
+                </li>
+                <li>
+                  <NavLink href="/change-logs">Changes</NavLink>
+                </li>
+                <li>
+                  <NavLink href="/contact-us">Contact</NavLink>
+                </li>
+              </div>
+              <li className="mr-1 ml-auto">
+                <a href={"https://app.fccareer.top"} target={"_blank"}>
                   <div className="go-to-app-button mr-4 h-16 flex items-center">
                     Go to App
                   </div>
