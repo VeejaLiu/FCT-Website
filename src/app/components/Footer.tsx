b87e6612c6555e132e15a67f6b96572fd3f6efc4
VeejaLiu <veejaliu@gmail.com>
2024-12-20 10:28:33 +0800

    refactor Footer component for improved styling and copyright notice

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,7 +3,14 @@
 const Footer = () => {
   return (
     <div className="flex flex-col items-center justify-center h-full w-full">
-      <div>Free, Automatic, Open-source</div>
+      <div className="text-lg font-bold my-2">
+        <span className="mx-2">Free</span>
+        <span className="mx-2">Automatic</span>
+        <span className="mx-2">Open-source</span>
+      </div>
+      <div className="text-sm text-gray-500">
+        Copyright FCT 2024. All rights reserved
+      </div>
     </div>
   );
 };
