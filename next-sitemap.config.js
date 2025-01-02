d1d79c157f26b3b9537fba05fcbeb6cdc34c21f4
VeejaLiu <veejaliu@gmail.com>
2025-01-02 11:57:27 +0800

    configure environment variables for site URL and add next-sitemap integration

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: process.env.SITE_URL || 'https://www.fccareer.top',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+}
